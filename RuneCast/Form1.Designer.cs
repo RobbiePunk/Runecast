42aaed72b41761880292bc10a1e18474818a83f4
unknown <chirva-roman@mail.ru>
2025-03-05 18:48:03 +0300

    добавлено распознавание цифр

---
+++ RuneCast/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RuneCast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.castingWindowPB = new System.Windows.Forms.PictureBox();
+            this.debugPB = new System.Windows.Forms.PictureBox();
+            this.countBT = new System.Windows.Forms.Button();
+            this.distanceBT = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.loadRuneBT = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FindBT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.castingWindowPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.debugPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // castingWindowPB
+            // 
+            this.castingWindowPB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.castingWindowPB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.castingWindowPB.Location = new System.Drawing.Point(24, 54);
+            this.castingWindowPB.Name = "castingWindowPB";
+            this.castingWindowPB.Size = new System.Drawing.Size(563, 558);
+            this.castingWindowPB.TabIndex = 0;
+            this.castingWindowPB.TabStop = false;
+            this.castingWindowPB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.StartCasting);
+            this.castingWindowPB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Casting);
+            this.castingWindowPB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.StopCasting);
+            // 
+            // debugPB
+            // 
+            this.debugPB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.debugPB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.debugPB.Location = new System.Drawing.Point(690, 54);
+            this.debugPB.Name = "debugPB";
+            this.debugPB.Size = new System.Drawing.Size(563, 558);
+            this.debugPB.TabIndex = 1;
+            this.debugPB.TabStop = false;
+            // 
+            // countBT
+            // 
+            this.countBT.Location = new System.Drawing.Point(593, 12);
+            this.countBT.Name = "countBT";
+            this.countBT.Size = new System.Drawing.Size(88, 23);
+            this.countBT.TabIndex = 2;
+            this.countBT.Text = "Save";
+            this.countBT.UseVisualStyleBackColor = true;
+            this.countBT.Click += new System.EventHandler(this.SaveRune);
+            // 
+            // distanceBT
+            // 
+            this.distanceBT.Location = new System.Drawing.Point(593, 63);
+            this.distanceBT.Name = "distanceBT";
+            this.distanceBT.Size = new System.Drawing.Size(88, 23);
+            this.distanceBT.TabIndex = 3;
+            this.distanceBT.Text = "Normalize";
+            this.distanceBT.UseVisualStyleBackColor = true;
+            this.distanceBT.Click += new System.EventHandler(this.distanceBT_Click);
+            // 
+            // loadRuneBT
+            // 
+            this.loadRuneBT.Location = new System.Drawing.Point(593, 120);
+            this.loadRuneBT.Name = "loadRuneBT";
+            this.loadRuneBT.Size = new System.Drawing.Size(88, 23);
+            this.loadRuneBT.TabIndex = 4;
+            this.loadRuneBT.Text = "Load";
+            this.loadRuneBT.UseVisualStyleBackColor = true;
+            this.loadRuneBT.Click += new System.EventHandler(this.LoadRune);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FindBT
+            // 
+            this.FindBT.Location = new System.Drawing.Point(593, 170);
+            this.FindBT.Name = "FindBT";
+            this.FindBT.Size = new System.Drawing.Size(88, 23);
+            this.FindBT.TabIndex = 5;
+            this.FindBT.Text = "Find";
+            this.FindBT.UseVisualStyleBackColor = true;
+            this.FindBT.Click += new System.EventHandler(this.FindBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 649);
+            this.Controls.Add(this.FindBT);
+            this.Controls.Add(this.loadRuneBT);
+            this.Controls.Add(this.distanceBT);
+            this.Controls.Add(this.countBT);
+            this.Controls.Add(this.debugPB);
+            this.Controls.Add(this.castingWindowPB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.castingWindowPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.debugPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox castingWindowPB;
+        private System.Windows.Forms.PictureBox debugPB;
+        private System.Windows.Forms.Button countBT;
+        private System.Windows.Forms.Button distanceBT;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button loadRuneBT;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button FindBT;
+    }
+}
+
---
+++ RuneCast/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuneCast
+{
+    public partial class Form1 : Form
+    {
+
+        bool isCasting = false;
+        int bias = 15;
+        int RuneSize = 400;
+
+        Bitmap castBmp;
+        Bitmap normalizedBmp;
+
+        Graphics castGR;
+        Graphics normalizedGR;
+
+        public List<Point> castedRune = new List<Point>();
+        public List<Point> loadedRune = new List<Point>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            castBmp = new Bitmap(castingWindowPB.Width, castingWindowPB.Height);
+            normalizedBmp = new Bitmap(debugPB.Width, debugPB.Height);
+
+            castGR = Graphics.FromImage(castBmp);
+            normalizedGR = Graphics.FromImage(normalizedBmp);
+        }
+
+        void Norm()
+        {
+            while (castedRune.Count < 50)
+            {
+                int ind = 1;
+
+                double maxDistance = 0;
+
+                for (int i = 1; i < castedRune.Count; i++)
+                {
+                    double dist = GetDistance(castedRune[i - 1], castedRune[i]);
+                    if (dist > maxDistance)
+                    {
+                        maxDistance = dist;
+                        ind = i;
+                    }
+                }
+                castedRune.Insert(ind,
+                    new Point((castedRune[ind - 1].X + castedRune[ind].X) / 2, (castedRune[ind - 1].Y + castedRune[ind].Y) / 2));
+            }
+
+            while (castedRune.Count > 50)
+            {
+                int ind = 1;
+
+                double minDistance = 10000;
+
+                for (int i = 1; i < castedRune.Count - 1; i++)
+                {
+                    double dist = GetDistance(castedRune[i - 1], castedRune[i]) + GetDistance(castedRune[i], castedRune[i + 1]);
+                    if (dist < minDistance)
+                    {
+                        minDistance = dist;
+                        ind = i;
+                    }
+                }
+                castedRune.RemoveAt(ind);
+            }
+
+            int maxY = 0;
+            int minY = castBmp.Height;
+            int maxX = 0;
+            int minX = castBmp.Width;
+
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                if (castedRune[i].X < minX)
+                    minX = castedRune[i].X;
+                if (castedRune[i].X > maxX)
+                    maxX = castedRune[i].X;
+                if (castedRune[i].Y < minY)
+                    minY = castedRune[i].Y;
+                if (castedRune[i].Y > maxY)
+                    maxY = castedRune[i].Y;
+            }
+
+            double coef = (double)RuneSize / (double)(Math.Max(maxX - minX, maxY - minY));
+
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                castedRune[i] = new Point((int)Math.Floor(coef * (double)(castedRune[i].X - minX)), (int)Math.Floor(coef * (double)(castedRune[i].Y - minY)));
+            }
+
+            return;
+
+            Point center = new Point(0, 0);
+
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                center = new Point(center.X + castedRune[i].X, center.Y + castedRune[i].Y);
+            }
+
+            center = new Point(center.X / castedRune.Count, center.Y / castedRune.Count);
+
+            Point shift = new Point(RuneSize / 2 - center.X, RuneSize / 2 - center.Y);
+
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                castedRune[i] = new Point(shift.X + castedRune[i].X, shift.Y + castedRune[i].Y);
+            }
+
+
+        }
+
+        double GetDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+
+        void DrawDebug(List<Point> rune)
+        {
+            SolidBrush BR = new SolidBrush(Color.Black);
+
+            normalizedGR.Clear(Color.White);
+
+            foreach(Point p in rune)
+                normalizedGR.FillRectangle(BR, p.X + bias, p.Y + bias, 4, 4);
+            debugPB.Image = normalizedBmp;
+
+        }
+
+        private void StartCasting(object sender, MouseEventArgs e)
+        {
+            castedRune.Clear();
+            castGR.Clear(Color.White);
+            isCasting = true;
+        }
+
+        private void StopCasting(object sender, MouseEventArgs e)
+        {
+            isCasting = false;
+            //NormalizeDistance();
+
+            DrawDebug(castedRune);
+        }
+
+        private void Casting(object sender, MouseEventArgs e)
+        {
+            if (!isCasting)
+                return;
+
+            Point newPoint = new Point(e.Location.X, e.Location.Y);
+
+            castedRune.Add(newPoint);
+
+            SolidBrush BR = new SolidBrush(Color.Black);
+
+            castGR.FillRectangle(BR, newPoint.X, newPoint.Y, 4, 4);
+            castingWindowPB.Image = castBmp;
+        }
+
+        private void SaveRune(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string SaveFileName = saveFileDialog1.FileName;
+
+                SaveCurrentRune(SaveFileName);
+                //label1.Text = saveTime;
+            }
+            //DrawDebug();
+        }
+
+        private void distanceBT_Click(object sender, EventArgs e)
+        {
+            //NormalizeDistance();
+            Norm();
+            DrawDebug(castedRune);
+        }
+
+        void SaveCurrentRune(string path)
+        {
+            StreamWriter sw = new StreamWriter(path);
+
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                sw.WriteLine(castedRune[i].X.ToString());
+                sw.WriteLine(castedRune[i].Y.ToString());
+            }
+
+            sw.Dispose();
+        }
+
+        private void LoadRune(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+
+                LoadRune(path);
+            }
+            DrawDebug(loadedRune);
+        }
+
+        void FindBestRune()
+        {
+            int minDif = 1000000000;
+            int ind = 0;
+
+            string path = Directory.GetCurrentDirectory() + "/Runes";
+
+            for (int i = 0; i < Directory.GetFiles(path).Length; i++)
+            {
+                LoadRune($"{path}/{i}");
+                int dif = CountDif();
+                if(dif < minDif)
+                {
+                    minDif = dif;
+                    ind = i;
+                }
+            }
+
+            LoadRune($"{path}/{ind}");
+            DrawDebug(loadedRune);
+        }
+
+        int CountDif()
+        {
+            int dif = 0;
+            for (int i = 0; i < castedRune.Count; i++)
+            {
+                dif += (int)GetDistance(castedRune[i], loadedRune[i]);
+            }
+
+            return dif;
+        }
+
+        void LoadRune(string path)
+        {
+            String[] Str = File.ReadAllLines(path);
+
+            loadedRune.Clear();
+            for (int i = 0; i < Str.Length / 2; i++)
+            {
+                loadedRune.Add(new Point(int.Parse(Str[2 * i]), int.Parse(Str[2 * i + 1])));
+            }
+        }
+
+        private void FindBT_Click(object sender, EventArgs e)
+        {
+            Norm();
+            FindBestRune();
+        }
+    }
+}
